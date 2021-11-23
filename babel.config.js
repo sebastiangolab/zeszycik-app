c3b7b68b2a72dd22ba602918dc88fc875842f5a7
sebastiangolab <sebagolab97@gmail.com>
2021-11-23 17:12:42 +0100

    Basic configured project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            components: './src/components',
+            assets: './src/assets/',
+            views: './src/views',
+          },
+        },
+      ],
+    ],
   };
 };
