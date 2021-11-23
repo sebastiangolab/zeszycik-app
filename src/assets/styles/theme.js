ccbcfb0bd02fe25c8d1c371ec6a1d9b715494b47
sebastiangolab <sebagolab97@gmail.com>
2021-11-23 17:50:09 +0100

    Add GlobalStyles and ThemeProvider

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primary: '#E2395F',
+    black: '#333',
+    white: '#fff',
+  },
+  fontSizes: {
+    headerTitle: '25px',
+    xl: '20px',
+    l: '18px',
+    m: '16px',
+    s: '13px',
+  },
+};
--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from 'assets/styles/GlobalStyles';
+import { theme } from '../assets/styles/theme';
 
 const App = () => {
-  return <h1>Hello Sebastian</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h1>Hello Sebastian</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
