92909fee6404c35a29ebd9ebf519d667c2f7f439
sebastiangolab <sebagolab97@gmail.com>
2022-01-15 00:37:07 +0100

    Fix back button on android

--- src/templates/MainTemplate/MainTemplate.js
+++ src/templates/MainTemplate/MainTemplate.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RoutesList } from '../../RoutesList';
 import { Header } from 'components/atoms/Header/Header';
 import { Menu } from 'components/organisms/Menu/Menu';
 import { BackgroundApp, Wrapper, Content } from './MainTemplate.styles';
 import { AddDebtButton } from 'components/molecules/AddDebtButton/AddDebtButton';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { BackHandler } from 'react-native';
 
 export const MainTemplate = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleBackButtonClick = () => {
+      navigate(-1)
+      return true;
+    }  
+    BackHandler.addEventListener('hardwareBackPress', handleBackButtonClick);
+  }, []);
 
   return (
     <BackgroundApp>
