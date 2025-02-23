822dca26d8b459e415633e267d2eaf0f732ad290
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-02-23 11:30:57 +0100

    Fix edit form validation

--- src/components/organisms/EditForm/EditForm.validation.js
+++ src/components/organisms/EditForm/EditForm.validation.js
@@ -3,12 +3,12 @@ import * as Yup from 'yup';
 export const ValidationSchema = Yup.object().shape({
   editValue: Yup.string().when('editActionForm', {
     is: (val) => ['add', 'delete'].includes(val),
-    then: Yup.string().required('Pole jest wymagane'),
+    then: (s) => s.required('Pole jest wymagane'),
   }),
 
   newName: Yup.string().when('editActionForm', {
     is: 'editName',
-    then: Yup.string().required('Pole jest wymagane'),
+    then: (s) => s.required('Pole jest wymagane'),
   }),
 
   description: Yup.string(),
